4e20595d41a7b1a7233e5a93a21df518d226c88a
Drew Covi <drew.covi@me.com>
2023-04-16 22:07:49 -0500

    cleanup

--- dist/settings.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLATFORM_NAME = exports.PLUGIN_NAME = void 0;
-exports.PLUGIN_NAME = 'homebridge-wled-presets';
-exports.PLATFORM_NAME = 'WLED Presets';
-//# sourceMappingURL=settings.js.map
